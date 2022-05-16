4d4b5b02ca0eba70d4a0253bae6a18f72fbb1db4
Borislav Landik <b.landik@ispsystem.com>
2022-05-16 12:14:52 +0800

    feat: 🎸 update theme switcher

--- src/scripts/canvas-3d/font-renderer.ts
+++ src/scripts/canvas-3d/font-renderer.ts
@@ -5,15 +5,58 @@ import TWEEN from '@tweenjs/tween.js';
 import { BaseRenderer } from './base-renderer';
 import { map } from '../utils';
 
+interface IColorSceme {
+    name: string;
+    mainColor: Color;
+    accentColor: Color;
+}
+
 export class FontRenderer extends BaseRenderer {
     private plane: Mesh = {} as Mesh;
     private meshes: Mesh[] = [];
     private pointer = new Vector3(0, 0, 0);
     private raycaster = new Raycaster();
 
-    private colorSceme: 'black' | 'white' = 'black';
-    private mainColor = 0x000000;
-    private fontHeight = 4;
+    private colorScemes: IColorSceme[] = [
+        {
+            name: 'matrix',
+            mainColor: new Color('#000000'),
+            accentColor: new Color('#13ed51'),
+        },
+        {
+            name: 'white-matrix',
+            mainColor: new Color('#ffffff'),
+            accentColor: new Color('#13ed51'),
+        },
+        {
+            name: 'blue',
+            mainColor: new Color('#0b1354'),
+            accentColor: new Color('#3491fa'),
+        },
+        {
+            name: 'pink',
+            mainColor: new Color('#ffd4dc'),
+            accentColor: new Color('#f765a3'),
+        },
+        {
+            name: 'coral',
+            mainColor: new Color('#fc766a'),
+            accentColor: new Color('#5b84b1'),
+        },
+        {
+            name: 'forest',
+            mainColor: new Color('#2c5f2d'),
+            accentColor: new Color('#97bc62'),
+        },
+        {
+            name: 'blazing-yellow',
+            mainColor: new Color('#101820'),
+            accentColor: new Color('#fee715'),
+        },
+    ];
+    private currentSceme: IColorSceme = this.colorScemes[0];
+
+    private fontHeight = 3;
     private fontSize = 5;
     
     constructor(canvasId: string) {
@@ -23,7 +66,13 @@ export class FontRenderer extends BaseRenderer {
         this.createBasePlane();
 
         const fontLoader = new FontLoader();
-        fontLoader.load('./assets/fonts/JetBrainsMonoExtraBold.json', (font) => this.generateText(font));
+        fontLoader.load('./assets/fonts/JetBrainsMonoExtraBoldFull.json', (font) => 
+            this.generateText(
+                font,
+                ['angular', 'react', 'vue', 'svelte'],
+                // ['<html>', '<script>', '<meta>', '#id', '.class', '[attr]', '@media', '@keyframes']
+            )
+        );
 
         this.startAnimation();
     }
@@ -51,7 +100,7 @@ export class FontRenderer extends BaseRenderer {
             position.y += this.fontSize / 2;
 
             const distance = position.distanceTo(intersectionPoint);
-            const zPosition = map(distance, 3, 10, this.fontHeight, 0);
+            const zPosition = map(distance, 1, 13, this.fontHeight, 0);
 
             mesh.position.setZ(Math.min(zPosition, 0));
         }
@@ -60,24 +109,37 @@ export class FontRenderer extends BaseRenderer {
     }
 
     public switchColorSceme(): void {
-        const mainColor = new Color(this.mainColor);
-        new TWEEN.Tween(mainColor)
-            .to(new Color(this.colorSceme === 'black' ? 0xffffff : 0x000000), 1000)
-            .easing(TWEEN.Easing.Quadratic.InOut)
+        const currentIndex = this.colorScemes.findIndex(f => f === this.currentSceme);
+        const nextSceme = this.colorScemes[(currentIndex + 1) % this.colorScemes.length];
+
+        const currentColors: IColorSceme = { 
+            ...this.currentSceme,
+            mainColor: this.currentSceme.mainColor.clone(),
+            accentColor:  this.currentSceme.accentColor.clone(),
+        };
+        const nextColors: IColorSceme = { 
+            ...nextSceme,
+            mainColor: nextSceme.mainColor.clone(),
+            accentColor:  nextSceme.accentColor.clone(),
+        };
+
+        new TWEEN.Tween(currentColors)
+            .to(nextColors, 1000)
+            .easing(TWEEN.Easing.Sinusoidal.Out)
             .onUpdate(() => {
                 this.meshes.forEach(mesh => {
                     const material = mesh.material as ShaderMaterial;
-                    material.uniforms.color2.value = mainColor;
+                    material.uniforms.color1.value = currentColors.accentColor;
+                    material.uniforms.color2.value = currentColors.mainColor;
                     material.needsUpdate = true;
                 });
         
                 const planeMaterial = this.plane.material as MeshBasicMaterial;
-                planeMaterial.color.set(mainColor);
+                planeMaterial.color.set(currentColors.mainColor);
                 planeMaterial.needsUpdate = true;
             })
             .onComplete(() => {
-                this.colorSceme = this.colorSceme === 'black' ? 'white' : 'black';
-                this.mainColor = mainColor.getHex();
+                this.currentSceme = nextSceme;
             })
             .start();
     }
@@ -93,10 +155,10 @@ export class FontRenderer extends BaseRenderer {
         const material = new ShaderMaterial({
             uniforms: {
                 color1: {
-                    value: new Color(0x20e831),
+                    value: this.currentSceme.accentColor,
                 },
                 color2: {
-                    value: new Color(this.mainColor),
+                    value: this.currentSceme.mainColor,
                 },
                 bboxMin: {
                     value: geometry.boundingBox?.min || 0,
@@ -135,7 +197,7 @@ export class FontRenderer extends BaseRenderer {
         return array[Math.floor(Math.random() * array.length)];
     }
 
-    private generateText(font: Font): void {
+    private generateText(font: Font, words: string[]): void {
         const screenWidth = window.screen.width;
         const screenHeight = window.screen.height;
 
@@ -147,17 +209,30 @@ export class FontRenderer extends BaseRenderer {
         const yStart = - overflowAddition; 
         const yEnd = Math.floor(screenHeight / (this.fontSize * cameraMultiplier)) + overflowAddition;
 
-        const textSymbols: Mesh[] = [
-            this.getTextMesh(font, '0'),
-            this.getTextMesh(font, '1'),
-        ];
+        const textMeshesMap: Map<string, Mesh> = new Map();;
+        for (const word of words) {
+            const symbols = word.split('');
+
+            for (const symbol of symbols) {
+                textMeshesMap.set(symbol, this.getTextMesh(font, symbol));
+            }
+        }
 
         for (let y = yStart; y <= yEnd; y += this.fontSize + 1) {
-            for (let x = xStart; x <= xEnd; x += this.fontSize - 1) {
-                const randomMesh = this.getRandomElement(textSymbols).clone();
-                randomMesh.position.set(x, y, this.fontHeight);
-                this.scene.add(randomMesh);
-                this.meshes.push(randomMesh);       
+            let x = xStart;
+
+            while (x <= xEnd) {
+                const randomWord = this.getRandomElement(words);
+
+                for (const symbol of randomWord) {
+                    const randomMesh = textMeshesMap.get(symbol)!.clone();
+                    randomMesh.position.set(x, y, this.fontHeight);
+
+                    this.scene.add(randomMesh);
+                    this.meshes.push(randomMesh);
+
+                    x += this.fontSize - 1;
+                }
             }
         }
     }
@@ -177,7 +252,7 @@ export class FontRenderer extends BaseRenderer {
 
     private createBasePlane(): void {
         const planeGeomerty = new PlaneGeometry(300, 300);
-        const planeMaterial = new MeshBasicMaterial({ color: this.mainColor });
+        const planeMaterial = new MeshBasicMaterial({ color: this.currentSceme.mainColor });
         this.plane = new Mesh(planeGeomerty, planeMaterial);
         this.plane.position.set(0, 0, 0);
         this.scene.add(this.plane);
